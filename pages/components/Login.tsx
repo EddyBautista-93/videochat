f91695411926c12e6f519490fccde0bce1b18a5c
EddyBautista <bautista.j.eddy@gmail.com>
2022-05-01 13:39:21 -0500

    downgrade to react 17 due to issues with @types with typescript, as well as breaking issue with 100ms

--- pages/components/Login.tsx
+++ pages/components/Login.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Room from "./Room";
 
 const Login: React.FunctionComponent = () => {
     return (
@@ -20,6 +21,7 @@ const Login: React.FunctionComponent = () => {
             </button>
           </div>
         </div>
+        <Room/>
         </>
     )
 }
